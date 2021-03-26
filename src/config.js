375d57722086eb72c34edac4314ad5032f23fd74
Roxanne <roxanne-m@users.noreply.github.com>
2021-03-26 15:00:53 -0700

    changing endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  // API_ENDPOINT: '(insert heroku link here)'
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://murmuring-fjord-17420.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
 
